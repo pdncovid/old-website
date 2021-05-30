30b58c9daf24da513bec535ae5cd0281c145688f
gihanjayatilaka <gihanjayatilaka@eng.pdn.ac.lk>
2021-05-30 22:21:34 +0530

    adding survey 1

--- generator/generate.sh
+++ generator/generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-arrPages=("about-us" "calendar" "links" "research" "publications" "news" "people" "join-us" "contact")
-arrPeople=("anuruddhika" "beshan" "drive" "erandi" "gayanthi" "gihan" "harshana" "insaf" "isuru" "janaka" "kaveen" "kavindu" "mevan" "navodini" "parakrama" "praditha" "rahal" "roshan" "rumali" "samath" "suren" "vijitha" "mallika" "sakunthala" "dilshan" "yasiru" "vision-draft")
+arrPages=("about-us" "calendar" "links" "research" "publications" "news" "people" "surveys" "join-us" "contact")
+arrPeople=("anuruddhika" "beshan" "drive" "erandi" "gayanthi" "gihan" "harshana" "insaf" "isuru" "janaka" "kaveen" "kavindu" "mevan" "navodini" "parakrama" "praditha" "rahal" "roshan" "rumali" "samath" "suren"  "survey1" "vijitha" "mallika" "sakunthala" "dilshan" "yasiru" "vision-draft")
 dateTime=$(date '+%d/%m/%Y %H:%M:%S (%:z)');
 
 generatePage(){
